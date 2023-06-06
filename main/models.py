16b412ac5d4ad06dad8bd05726bf9d74b5bf9bad
Denis Seredin <sibwinter42@gmail.com>
2023-06-06 16:33:21 +0300

    add something

--- main/models.py
+++ main/models.py
@@ -85,7 +85,7 @@ class Radiator():
         result = self.L_rib_length * self.l_radiator_length
         self.square_of_non_ribbed_surface = result
 
-    def square_of_ribbed_surfase(self) ->float:
+    def square_of_ribbed_surfase(self) -> float:
         """Определяем площадь оребренной поверхности радиатора.
         
         при креплении ППП с гладкой стороны, Sор1, м2;
@@ -97,3 +97,75 @@ class Radiator():
         square_2 = (self.sigma_plate + 2 * self.h_rib_height) * self.L_rib_length * self.n_number_of_edges + 2 * self.l_radiator_length * self.sigma_plate
         square_3 = 2 * self.n_number_of_edges * self.sigma_plate * self.h_rib_height
         return square_1 + square_2 + square_3
+
+    def alfa_convection_determine(self) -> float:
+        """Определяем коэффициент теплоотдачи конвекцией для гладкой поверхности радиатора.
+
+        aк.гл, Вт/м2*град;
+        aк.гл=А1· [(Тр-Тс)/2]^(1/4)
+        где А1 определяется по формуле:
+        А1=1,424767–0,00251 ·Тм+0,000011 · (Тм)^2-0,0000000013 · (Тм)^3
+        Тм=0,5 (Тр+Тс)"""
+
+        Tm = 0.5 * (self.t_average() + self.t_outside)
+        A1 = 1,424767 - 0.00251 * Tm + 0.000011 * Tm ^ 2 - 0.0000000013 * Tm ^ 3
+        alfa_convection = A1 * ((self.t_average() - self.t_outside)/2) ^ (1/4)
+        return alfa_convection
+
+    def alfa_radiation_determine(self) -> float:
+        """Определяем коэффициент теплоотдачи излучения для гладкой поверхности радиатора, л.гл, Вт/м2*град.
+
+        Alfaл.гл=ε ·φ ·₣(Тр, Тс), (14)
+        где ε – степень черноты тела (для Д‑16 ε=0,4);
+        φ – коэффициент облучённости (для гладкой поверхности φ=1);
+        ₣(Тр, Тс) – рассчитывается по формуле:
+        ₣(Тр, Тс)=5,67 ·10-8 · [(Тр+267)4 – (Тс+267)4]/(Тр-Тс) (15)"""
+        epsi = 0.4  # задаем степень черноты тела
+        F = 5.67 * 10 ^ (-8) * ((self.t_average() + 267) ^ 4 - (self.t_outside + 267) ^ 4)/(self.t_average() - self.t_outside)
+        alfa_radiation = epsi * 1 * F
+        return alfa_radiation
+
+    def alfa_summary_determine(self) -> float:
+        """Определяем эффективный коэффициент теплоотдачи гладкой поверхности радиатора, гл, Вт/м2*град;.
+
+        Alfaгл = Alfaк.гл + Alfaл.гл"""
+        return self.alfa_convection_determine() + self.alfa_radiation_determine()
+
+    def power_of_smooth_surface_define(self) -> float:
+        """Определяем мощность, рассеиваемая гладкой поверхностью радиатора, Ргл, Вт.
+
+        Ргл=гл·Sгл· (Тр-Тс)"""
+        power_of_smooth_surface = self.alfa_summary_determine() * self.square_of_non_ribbed_surface() * (self.t_average() - self.t_outside)
+        return power_of_smooth_surface
+
+    def resistance_of_smooth_surface_define(self) -> float:
+        """15) Определяем тепловое сопротивление гладкой поверхности радиатора, Rгл, град / Вт;.
+        Rгл=1/(Alfaгл·Sгл) (18)"""
+        resistance_of_smooth_surface = 1 / (self.alfa_summary_determine * self.square_of_non_ribbed_surface())
+        return resistance_of_smooth_surface
+
+    def koef_temp_pressure_define(self):
+        """Определяем коэффициенты для нахождения относительного температурного напора.
+
+        C=K/M.
+        nu = A2(Tm)*b*C
+        Здесь какая то откровенная магия. Оставляю эти коэффициенты как внешние данные.
+        """
+        self.nu = float(input())
+
+    def H_temp_pressure_define(self) -> float:
+        """Определяем относительный температурный напор Н.
+
+        напор определяется по магическому графику исходя из коэффициента nu"""
+
+        self.temp_pressure = float(input())
+        return self.temp_pressure
+
+    def ambient_temp_between_edges(self) -> float:
+        """Определяем температуру окружающей среды между рёбрами, Тс1, °С;.
+
+        Тс1=(Тр+Тс)/2
+        """
+        result = (self.t_average - self.t_outside) / 2
+        return result
+
